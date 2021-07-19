d4913040460082eec64e87624044cb849da93abc
gzocchi <z.giorgio.z@gmail.com>
2021-07-19 14:53:23 +0200

    modificato comics migration

--- database/migrations/2021_07_19_124255_create_comics_table.php
+++ database/migrations/2021_07_19_124255_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 60);
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price', 6, 2); // max 9999.99
+            $table->string('series', 60);
+            $table->string('sale_date', 10);
+            $table->string('type', 30);
             $table->timestamps();
         });
     }
